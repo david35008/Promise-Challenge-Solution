81e25cd4b8b8ff9b7c9dae0f1e36724feea5ec82
david35008 <67857450+david35008@users.noreply.github.com>
2020-10-05 15:16:32 +0300

    finish

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-class Declare{
-  //implement here
+class Declare {
+  constructor(CB = () => { }) {
+    this.state = 'pending'
+    this.value = null
+    this.resolver = () => { }
+    CB(this.resolve.bind(this))
+  }
+
+  resolve(value) {
+    if (this.state === 'pending') {
+      this.state = 'resolved';
+      this.value = value
+      this.resolver(value)
+    }
+  }
+
+  then(CB = () => { }) {
+    this.resolver = CB
+    if (this.state === 'resolved') {
+      return new Declare((resolveCB) => {
+        resolveCB(CB(this.value))
+      })
+    } else {
+      return this
+    }
+  }
 }
 module.exports = Declare
